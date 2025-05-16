2f708d234d4bb6d97f0c37f1225b7aec7f11a745
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-16 23:31:43 +0300

    Add logging & OpenTelemetry

---
+++ CurrencyConverter.API/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace CurrencyConverter.API.Middlewares
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            var clientIp = context.Connection.RemoteIpAddress?.ToString();
+            var httpMethod = context.Request.Method;
+            var endpoint = $"{context.Request.Path}{context.Request.QueryString}";
+            var responseCode = context.Response.StatusCode;
+            
+            var stopwatch = Stopwatch.StartNew();
+            await _next(context);
+            stopwatch.Stop();
+
+            var responseTimeMs = stopwatch.ElapsedMilliseconds;
+
+            _logger.LogInformation("HTTP Request {@Method} {@Endpoint} responded {@StatusCode} in {@ElapsedMs}ms | ClientIP: {@ClientIP}",
+                httpMethod, endpoint, responseCode, responseTimeMs, clientIp);
+        }
+    }
+}
--- CurrencyConverter.API/Program.cs
+++ CurrencyConverter.API/Program.cs
@@ -5,6 +5,12 @@ using CurrencyConverter.Core.Services;
 using CurrencyConverter.Core.Abstractions;
 using CurrencyConverter.Infrastructure.ExchangeRateProviders.Frankfurter;
 using Polly;
+using CurrencyConverter.API.Middlewares;
+using Serilog;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
 
 namespace CurrencyConverter.API
 {
@@ -14,25 +20,23 @@ namespace CurrencyConverter.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            Log.Logger = new LoggerConfiguration()
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console()
+                            .WriteTo.Seq("http://localhost:5341")
+                            .CreateLogger();
+
+
+            #region Extenstion Methods
+            builder.Logging.AddOpenTelemetry();
+            builder.Services.AddOpenTelemetryService();
+            builder.Services.AddVersioning();
+            #endregion
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            builder.Services.AddApiVersioning(options =>
-            {
-                options.AssumeDefaultVersionWhenUnspecified = true;
-                options.DefaultApiVersion = new ApiVersion(1, 0);
-                options.ApiVersionReader = ApiVersionReader.Combine(
-                    new HeaderApiVersionReader("x-api-version"),
-                    new QueryStringApiVersionReader("api-version"),
-                    new UrlSegmentApiVersionReader()
-                );
-            });
-
-            
 
             builder.Services.AddScoped<ExchangeRateService>();
             builder.Services.AddScoped<IExchangeRateProvider, FrankfurterExchangeRateProvider>();
@@ -54,13 +58,57 @@ namespace CurrencyConverter.API
             }
 
             app.UseHttpsRedirection();
+            
+            app.UseMiddleware<RequestLoggingMiddleware>();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
             app.Run();
+        }
+
+
+    }
+
+    public static class Extenstions
+    {
+        public static ILoggingBuilder AddOpenTelemetry(this ILoggingBuilder loggingBuilder)
+        {
+            return loggingBuilder.AddOpenTelemetry(options =>
+            {
+                options
+                    .SetResourceBuilder(
+                        ResourceBuilder.CreateDefault()
+                            .AddService("CurrencyConverter"))
+                    .AddConsoleExporter();
+            });
+        }
+
+        public static void AddOpenTelemetryService(this IServiceCollection services)
+        {
+             services.AddOpenTelemetry()
+                    .ConfigureResource(resource => resource.AddService("CurrencyConverter"))
+                    .WithTracing(tracing => tracing
+                            .AddAspNetCoreInstrumentation()
+                            .AddConsoleExporter())
+                    .WithMetrics(metrics => metrics
+                        .AddAspNetCoreInstrumentation()
+                        .AddConsoleExporter());
+        }
+
+        public static IServiceCollection AddVersioning(this IServiceCollection services)
+        {
+           return services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("x-api-version"),
+                    new QueryStringApiVersionReader("api-version"),
+                    new UrlSegmentApiVersionReader()
+                );
+            });
         }
     }
 }
