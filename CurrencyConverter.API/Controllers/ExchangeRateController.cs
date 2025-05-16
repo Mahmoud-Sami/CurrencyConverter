f6e4b0f21ef76fab520fed7b5a458cad9112db8e
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-16 16:15:24 +0300

    Implement get latest exchange rate API v1

--- CurrencyConverter.API/Controllers/ExchangeRateController.cs
+++ CurrencyConverter.API/Controllers/ExchangeRateController.cs
@@ -1,6 +1,30 @@
-﻿namespace CurrencyConverter.API.Controllers
+﻿using System.ComponentModel.DataAnnotations;
+using CurrencyConverter.Core.Abstractions;
+using CurrencyConverter.Core.Entities;
+using CurrencyConverter.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverter.API.Controllers
 {
-    public class ExchangeRatesController
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/exchange-rates")]
+    public class ExchangeRatesController : ControllerBase
     {
+        private readonly ExchangeRateService _exchangeRateService;
+
+        public ExchangeRatesController(ExchangeRateService exchangeRateService)
+        {
+            _exchangeRateService = exchangeRateService;
+        }
+
+        [HttpGet("latest")]
+        public async Task<ActionResult<ExchangeRate>> GetLatestRates(
+            [FromQuery, Required] string baseCurrency,
+            CancellationToken cancellationToken)
+        {
+            ExchangeRate exchangeRate = await _exchangeRateService.GetLatestRatesAsync(baseCurrency, cancellationToken);
+            return Ok(exchangeRate);
+        }
     }
 }
--- CurrencyConverter.API/Program.cs
+++ CurrencyConverter.API/Program.cs
@@ -1,4 +1,10 @@
 
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using CurrencyConverter.Core.Services;
+using CurrencyConverter.Core.Abstractions;
+using CurrencyConverter.Infrastructure.ExchangeRateProviders.Frankfurter;
+
 namespace CurrencyConverter.API
 {
     public class Program
@@ -13,6 +19,20 @@ namespace CurrencyConverter.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("x-api-version"),
+                    new QueryStringApiVersionReader("api-version"),
+                    new UrlSegmentApiVersionReader()
+                );
+            });
+
+            builder.Services.AddScoped<ExchangeRateService>();
+            builder.Services.AddScoped<IExchangeRateProvider, FrankfurterExchangeRateProvider>();
 
             var app = builder.Build();
 
